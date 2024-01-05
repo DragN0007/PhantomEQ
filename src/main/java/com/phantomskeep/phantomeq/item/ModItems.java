360fd2fd6565dd762674429960040f8d7ae7d6bd
DragN0007 <cooper.kylie@gmail.com>
2024-01-04 19:43:17 -0600

    -Fix Water Trough Display

--- src/main/java/com/phantomskeep/phantomeq/item/ModItems.java
+++ src/main/java/com/phantomskeep/phantomeq/item/ModItems.java
@@ -2,15 +2,9 @@ package com.phantomskeep.phantomeq.item;
 
 
 import com.phantomskeep.phantomeq.PhantomEQ;
-import com.phantomskeep.phantomeq.block.ModBlocks;
-import com.phantomskeep.phantomeq.entity.ModEntities;
-import com.phantomskeep.phantomeq.entity.util.EntityTypes;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SaddleItem;
-import net.minecraftforge.common.ForgeSpawnEggItem;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,7 +12,6 @@ import org.jetbrains.annotations.NotNull;
 
 
 public class ModItems {
-
     public static final DeferredRegister<Item> ITEM_DEFERRED =
             DeferredRegister.create(ForgeRegistries.ITEMS, PhantomEQ.MODID);
 
@@ -37,17 +30,32 @@ public class ModItems {
             () -> new Item(new Item.Properties().stacksTo(64).tab(ModItems.PEQ)));
 
 
+
+    //MODTAB ICONS
+    public static final RegistryObject<Item> MODTAB_1 = ITEM_DEFERRED.register("modtab_1",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MODTAB_2 = ITEM_DEFERRED.register("modtab_2",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MODTAB_3 = ITEM_DEFERRED.register("modtab_3",
+            () -> new Item(new Item.Properties()));
+
+    //MODTAB
     public static final CreativeModeTab PEQ = new CreativeModeTab("PEQ") {
         @Override
         public @NotNull ItemStack makeIcon() {
-            return new ItemStack(ModItems.PEQ_SADDLE.get());
+            return new ItemStack(ModItems.MODTAB_1.get());
         }
     };
 
     public static final CreativeModeTab DECOR = new CreativeModeTab("DECOR") {
         @Override
         public @NotNull ItemStack makeIcon() {
-            return new ItemStack(ModItems.REGULAR_FEED.get());
+            return new ItemStack(ModItems.MODTAB_2.get());
         }
+    };
+
+    public static final CreativeModeTab FOOD = new CreativeModeTab("FOOD") {
+        @Override
+        public ItemStack makeIcon() { return new ItemStack(ModItems.MODTAB_3.get()); }
     };
 }
